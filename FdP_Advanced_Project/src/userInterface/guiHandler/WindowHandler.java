53814d6525dc71497fc3177016106b430d796a6f
Manuel Gallina <mgallina96@gmail.com>
2016-06-18 18:31:38 +0200

    Add dropdown menu and changed ui style.

--- FdP_Advanced_Project/src/userInterface/guiHandler/WindowHandler.java
+++ FdP_Advanced_Project/src/userInterface/guiHandler/WindowHandler.java
@@ -1,9 +1,16 @@
 package userInterface.guiHandler;
 
 import content.Patient;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.HPos;
+import javafx.geometry.Orientation;
+import javafx.geometry.VPos;
 import javafx.scene.Scene;
 import javafx.scene.SubScene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.Separator;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
@@ -34,6 +41,8 @@ public class WindowHandler
 	private static final String INFO_BOX_TITLE = "Info paziente";
 	private static final double INFO_BOX_WIDTH_RESOLUTION = MAIN_WINDOW_WIDTH_RESOLUTION - SCENE3D_WIDTH_RESOLUTION;
 	private static final double INFO_BOX_HEIGHT_RESOLUTION = MAIN_WINDOW_HEIGHT_RESOLUTION;
+	
+	private static final double SEPARATOR_MAX_WIDTH = INFO_BOX_WIDTH_RESOLUTION -20;
 	
 	private HBox root;
 	private Scene scene;
@@ -81,11 +90,24 @@ public class WindowHandler
 	private VBox lateralWindow(Patient patient)
 	{
 		VBox box = new VBox();
+			box.setSpacing(10);
+		
 		VBox infoBox = infoBox(patient);
+			infoBox.setId("infoBox");
+				
+		Separator separator = new Separator();
+			separator.setId("separator");
+			separator.setOrientation(Orientation.HORIZONTAL);
+			separator.setMaxWidth(SEPARATOR_MAX_WIDTH);
+			separator.setHalignment(HPos.CENTER);
+			separator.setValignment(VPos.CENTER);
 		
 		box.setPrefSize(INFO_BOX_WIDTH_RESOLUTION, INFO_BOX_HEIGHT_RESOLUTION);
 		
-		box.getChildren().add(infoBox);
+		VBox toolSelection = toolSelection();
+			toolSelection.setId("toolSelection");
+		
+		box.getChildren().addAll(infoBox, separator, toolSelection);
 		
 		return box;
 	}
@@ -98,26 +120,52 @@ public class WindowHandler
 		VBox box = new VBox();
 		
 		Label title = new Label(INFO_BOX_TITLE);
-		title.setId("infoBoxTitle");
 		
 		GridPane grid = new GridPane();
+			grid.setId("infoBoxGrid");
+		
 		Label[] attributes = new Label[patient.getPatientAttributes().length];
 		Label[] data = new Label[patient.getPatientInfo().length];
 		
 		for(int i = 0; i < patient.getPatientAttributes().length; i++)
 		{
 			attributes[i] = new Label(patient.getPatientAttributes()[i]);
-			attributes[i].setId("infoBoxLabels");
 			
 			data[i] = new Label(patient.getPatientInfo()[i]);
-			data[i].setId("InfoBoxData");
 			
 			grid.add(attributes[i], 0, i);
 			grid.add(data[i], 1, i);
 		}
 		
-		box.getChildren().add(title);
-		box.getChildren().add(grid);
+		box.getChildren().addAll(title, grid);
+		
+		return box;
+	}
+
+	private VBox toolSelection() 
+	{
+		VBox box = new VBox();
+	
+		Label title = new Label(INFO_BOX_TITLE);
+	
+		GridPane grid = new GridPane();
+			grid.setId("infoBoxGrid");
+	
+		ObservableList<String> options = 
+			    FXCollections.observableArrayList(
+			        "Option 1",
+			        "Option 2",
+			        "Option 3"
+			    );
+
+		
+		ComboBox<String> tool1 = new ComboBox<String>(options);
+		ComboBox<String> tool2 = new ComboBox<String>(options);
+		
+		grid.add(tool1, 0, 0);
+		grid.add(tool2, 0, 1);
+		
+		box.getChildren().addAll(title, grid);
 		
 		return box;
 	}
